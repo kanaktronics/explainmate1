2ffe9666287c1681537227d87afe8fd1a6aaaa04
Drop&Shop <dropandshop10@gmail.com>
2025-12-02 15:23:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5165217038-21de7",
+  "appId": "1:1049244963692:web:54c663633bbce0c5b35949",
+  "apiKey": "AIzaSyDSyrAcDCMYYI-ZipIARSfEjvoJtMAtvfE",
+  "authDomain": "studio-5165217038-21de7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1049244963692"
+};
