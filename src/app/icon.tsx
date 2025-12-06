1831fc566cadb2b929629ed50dc96c5721d89cb3
Drop&Shop <dropandshop10@gmail.com>
2025-12-06 15:29:59 +0000

    see its showing firebase logo which i dont want

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -25,18 +25,22 @@ export default function Icon() {
         }}
       >
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         >
-          <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-          <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+            <path d="M12 2a10 10 0 0 0-2 19.5v-1.5a8 8 0 1 1 4 0v1.5A10 10 0 0 0 12 2Z" />
+            <path d="M12 13a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" />
+            <path d="M12 13v1.5" />
+            <path d="M10 14.5a4 4 0 0 0-1.5 2.5" />
+            <path d="M15.5 17a4 4 0 0 0-2.5-1.5" />
+            <path d="M12 21.5V19" />
         </svg>
       </div>
     ),
