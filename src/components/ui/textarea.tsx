61e0c9783cb5ed78d17740c9c9b9773721d14bf9
Drop&Shop <dropandshop10@gmail.com>
2025-12-06 09:14:40 +0000

    no scrolling its not resolved

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
+import { useAutosizeTextArea } from '@/hooks/use-autosize-textarea';
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
   ({className, ...props}, ref) => {
+    const internalRef = React.useRef<HTMLTextAreaElement>(null);
+    React.useImperativeHandle(ref, () => internalRef.current as HTMLTextAreaElement);
+    
+    useAutosizeTextArea(internalRef.current, props.value as string);
+
     return (
       <textarea
         className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 overflow-hidden',
           className
         )}
-        ref={ref}
+        ref={internalRef}
         {...props}
       />
     );
